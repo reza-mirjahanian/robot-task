a71edf32fa1dc2a4e4decfa37b05cfcae964e697
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-10-03 10:03:24 +0330

    Add jsdoc to Table class

--- src/classes/Table.ts
+++ src/classes/Table.ts
@@ -1,39 +1,64 @@
 export default class Table {
 
-  private _locationX = -1
-  private _locationY = -1
-  private _isActive = false;
+  private _objectX = -1
+  private _objectY = -1
+  private _isObjectActive = false;
 
-  constructor(private _width: number = 5, private _height: number = 5) {
+  /**
+   * @constructor
+   * @param {number} _width - The width of the table.
+   * @param {number} _height - The height of the table.
+   */
+  constructor(private _width: number = 5, private _height: number = 5) {}
 
-  }
 
+  /**
+   * is location in the board?
+   * @param {number} x
+   * @param {number} y
+   * @return {boolean}
+   */
   private _isValidPlace(x: number, y: number) {
     if (x >= 0 && x < this._width) {
       if (y >= 0 && y < this._height) {
-        this._isActive = true;
         return true;
       }
     }
     return false
   }
 
-  setLocation(x: number, y: number) {
+
+  /**
+   * Put Robot on the table and set the _isObjectActive true for correct locations.
+   * @param {number} x
+   * @param {number} y
+   */
+  putObject(x: number, y: number) {
     if (this._isValidPlace(x, y)) {
-      this._locationX = x;
-      this._locationY = y;
+      this._isObjectActive = true;
+      this._objectX = x;
+      this._objectY = y;
     }
   }
 
-  getLocation() {
+
+  /**
+   * Return the Robot location on the table.
+   * @return {x: number, y: number}
+   */
+  getObjectLocation() {
     return {
-      x: this._locationX,
-      y: this._locationY
+      x: this._objectX,
+      y: this._objectY
     }
   }
 
-  public get isActive() {
-    return this._isActive;
+  /**
+   * Is table active? (Robot is in the correct place).
+   * @type {boolean}
+   */
+  public get isObjectActive() {
+    return this._isObjectActive;
   }
 
 }
--- test/Table.spec.ts
+++ test/Table.spec.ts
@@ -9,18 +9,18 @@ suite('Testing Table Class', () => {
   suite(':Locations', () => {
     test('should set location correctly ', async () => {
       const table = new Table()
-      table.setLocation(1,2)
-      const {x,y} = table.getLocation();
+      table.putObject(1,2)
+      const {x,y} = table.getObjectLocation();
       expect(x).to.be.equal(1);
       expect(y).to.be.equal(2);
 
-      table.setLocation(4,0)
-      const {x: x2,y: y2} = table.getLocation();
+      table.putObject(4,0)
+      const {x: x2,y: y2} = table.getObjectLocation();
       expect(x2).to.be.equal(4);
       expect(y2).to.be.equal(0);
 
-      table.setLocation(0,4)
-      const {x: x3,y: y3} = table.getLocation();
+      table.putObject(0,4)
+      const {x: x3,y: y3} = table.getObjectLocation();
       expect(x3).to.be.equal(0);
       expect(y3).to.be.equal(4);
 
@@ -28,19 +28,19 @@ suite('Testing Table Class', () => {
 
     test('should active for correct location', async () => {
       const table = new Table()
-      table.setLocation(5,5)
-      expect(table.isActive).to.be.equal(false);
+      table.putObject(5,5)
+      expect(table.isObjectActive).to.be.equal(false);
 
-      table.setLocation(-1,5)
-      expect(table.isActive).to.be.equal(false);
-
-
-      table.setLocation(0,0)
-      expect(table.isActive).to.be.equal(true);
+      table.putObject(-1,5)
+      expect(table.isObjectActive).to.be.equal(false);
 
 
-      table.setLocation(-1,5)
-      expect(table.isActive).to.be.equal(true);
+      table.putObject(0,0)
+      expect(table.isObjectActive).to.be.equal(true);
+
+
+      table.putObject(-1,5)
+      expect(table.isObjectActive).to.be.equal(true);
 
 
     });
