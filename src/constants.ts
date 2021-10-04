3501759bae7d3ba6215771eae7975fb36fa89c44
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-10-04 09:03:44 +0330

    Add test for wrong command

--- src/constants.ts
+++ src/constants.ts
@@ -8,5 +8,6 @@ const
 export default {
     TEST_DATA_PATH: process.env.TEST_DATA_PATH || TEST_DATA_PATH,
     TEST_DATA_ONE: process.env.TEST_DATA_ONE || path.resolve(TEST_DATA_PATH,'commands1.txt'),
+    TEST_DATA_ONE_WRONG: process.env.TEST_DATA_ONE_WRONG || path.resolve(TEST_DATA_PATH,'wrong-commands1.txt'),
 };
 
--- test/CommandParser.spec.ts
+++ test/CommandParser.spec.ts
@@ -51,12 +51,22 @@ suite('Testing CommandParser Class', () => {
 
     });
 
-    test('should handle errors correctly ',  () => {
+    test('should handle errors(wrong file path) correctly ',  () => {
       const commandParser = new CommandParser("./wrong");
       const cmdIterator = commandParser.parse();
-      sinon.spy(console, 'error');
+      const spyHandle = sinon.spy(console, 'error');
       cmdIterator.next()
       expect(console.error).to.be.calledWith("#Error: ENOENT: no such file or directory, open './wrong'");
+      spyHandle.restore();
+    });
+
+    test('should handle errors(typo in command) correctly ',  () => {
+      const commandParser = new CommandParser(constants.TEST_DATA_ONE_WRONG);
+      const cmdIterator = commandParser.parse();
+      const spyHandle = sinon.spy(console, 'error');
+      cmdIterator.next()
+      expect(console.error).to.be.calledWith("#Error: Invalid command: 1PLACE 1,2,EAST");
+      spyHandle.restore();
     });
 
   });
